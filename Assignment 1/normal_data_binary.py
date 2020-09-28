3c3be2e81445334f164bee1e4b8ae3b464e80ea7
omkar1610 <omkar.ashrit@gmail.com>
2020-09-28 20:29:26 +0530

    'auto commit vs'

--- Assignment 1/normal_data_binary.py
+++ Assignment 1/normal_data_binary.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def generate_random_points(dim=2, class_count=[50, 60], mean=10, var=2, step=100):
     '''
     This function simply generates normal points of 'mean' and 'variance', and keep on adding a bias of size 'step' for each class.
@@ -20,20 +21,21 @@ def generate_random_points(dim=2, class_count=[50, 60], mean=10, var=2, step=100
         dim = 3 i.e each point is of 3 dimension
         class_count = [10, 20, 15] means 10 points of class 0, 20 points of class 1 and 15 points of class 2
     '''
-    x = np.random.normal(mean, var, size=[class_count[0],dim])
+    x = np.random.normal(mean, var, size=[class_count[0], dim])
     y = [0 for i in range(class_count[0])]
     i = 1
     for n in class_count[1:]:
-        tmp = np.random.normal(10, 2, size=[n,dim]) + i * step
+        tmp = np.random.normal(10, 2, size=[n, dim]) + i * step
         i += 1
         x = np.vstack((x, tmp))
         y = np.hstack((y, [i-1 for j in range(n)]))
     return x, y
 
+
 def plot_2d(x, y, w, b):
     df = pd.DataFrame(x)
     df['y'] = y
-    
+
     plt.scatter(df[0], df[1])
 
     xa = np.linspace(0, 200, 2)
@@ -41,24 +43,23 @@ def plot_2d(x, y, w, b):
     ya = (w[0]*xa+b)/(-w[1])
     plt.plot(xa, ya, 'r')
 
-    
     plt.grid(1)
     plt.show()
-    
+
 
 if __name__ == '__main__':
-    X, Y = generate_random_points(class_count=[500, 500,])
+    X, Y = generate_random_points(class_count=[500, 500, ])
     print("Random 2D Normally distributed Points Generated...\n")
-    
+
     # split the data to train test with 80:20 ratio
     X_tr, X_ts, Y_tr, Y_ts = train_test_split(X, Y, test_size=0.2)
-    
+
     # perceptron model
     model = Perceptron(shuffle=1, verbose=0)
 
     # converting to  list as the perceptron model doesn't recognize np arrays.
     mat = model.fit(X_tr, Y_tr)
-    
+
     print("\nThe Weight and Bias :")
     model.display_weight()
     print("\nAccuracy : ")
@@ -69,5 +70,3 @@ if __name__ == '__main__':
         plot_2d(X, Y, model.w, model.b)
     else:
         print("Not plotting as the data is not 2D")
-
-    
--- Assignment 1/perceptron.py
+++ Assignment 1/perceptron.py
@@ -1,8 +1,9 @@
 import random
 
+
 class Perceptron:
     def __init__(self, fit_intercept=True, max_iter=1000, l_rate=1.0,
-                tol=0.003, shuffle=True, verbose=False):
+                 tol=0.003, shuffle=True, verbose=False):
         # weight and bias
         self.w = None
         self.b = None
@@ -10,7 +11,7 @@ class Perceptron:
         # no of iterations and no of class lables
         self.n_iter = 0
         self.classes = None
-        
+
         # whether to set b == 0 or not(for this it is fixed)
         self.fit_intercept = fit_intercept
 
@@ -24,8 +25,8 @@ class Perceptron:
         self.shuffle = shuffle
         self.verbose = verbose
 
-        print("Perceptron Model Parameters = (fit_intercept=", self.fit_intercept, ", max_iter=", self.max_iter , ", learning_rate=", self.l_rate,
-                "tolerence=", self.tol, ", shuffle=", self.shuffle, ", verbose=", self.verbose, ")\n")
+        print("Perceptron Model Parameters = (fit_intercept=", self.fit_intercept, ", max_iter=", self.max_iter, ", learning_rate=", self.l_rate,
+              "tolerence=", self.tol, ", shuffle=", self.shuffle, ", verbose=", self.verbose, ")\n")
 
     def __dot(self, w, x):
         # computes __dot product of w and x
@@ -34,12 +35,12 @@ class Perceptron:
     def __display_weight_binary(self, w=None, b=None):
         # prints the w and b for binary classes
         if w and b:
-            print("W = ", ["{0:0.2f}".format(i) for i in  w])
-            print("B = ", "{0:0.2f}".format(b)) 
+            print("W = ", ["{0:0.2f}".format(i) for i in w])
+            print("B = ", "{0:0.2f}".format(b))
         else:
-            print("W = ", ["{0:0.2f}".format(i) for i in  self.w])
-            print("B = ", "{0:0.2f}".format(self.b)) 
-            
+            print("W = ", ["{0:0.2f}".format(i) for i in self.w])
+            print("B = ", "{0:0.2f}".format(self.b))
+
     def __display_weight_multiclass(self, w=None, b=None):
         # prints the w and b for multiclass
 
@@ -49,36 +50,36 @@ class Perceptron:
         else:
             print("W = ", [["{0:0.2f}".format(i) for i in j] for j in self.w])
             print("B = ", ["{0:0.2f}".format(i) for i in self.b])
-        
+
     def __predict_binary(self, x):
-        # compute the value of dot(w, x)+b 
-        return 1.0 if self.__dot(self.w, x)+self.b>=0.0 else 0.0     
-        
+        # compute the value of dot(w, x)+b
+        return 1.0 if self.__dot(self.w, x)+self.b >= 0.0 else 0.0
+
     def __predict_multiclass(self, x):
         mat = []
         for i in range(len(self.w)):
             mat.append(self.__dot(self.w[i], x)+self.b[i])
-        
+
         # returns the max dot product class assuming the classes are from 0 to c-1
         return mat.index(max(mat))
-               
+
     def __fit_binary(self, X, Y):
         # saves a copy of weights for each epoch
         wt_matrix = []
         self.data = list(zip(X, Y))
-        
+
         # Initialise the weight and bias randomly
         self.w = [random.random() for i in range(len(X[0]))]
         self.b = random.random()
-        
+
         # set the loss to be maximum as initial value
         self.min_loss = len(X)
-        
+
         # save the weight and bias that gives the minimum loss(no of misclassified points)
         self.checkw = self.w
         self.checkb = self.b
         self.loss = 0
-        
+
         if self.verbose:
             print("Initialised W and B are: ")
             self.__display_weight_binary(self.w, self.b)
@@ -86,33 +87,34 @@ class Perceptron:
             print("Value Initialised. Starting the epochs...\n")
 
         for epoch in range(self.max_iter):
-            
+
             # shuffle the input randomly
             if self.shuffle:
                 random.shuffle(self.data)
-                
-            self.loss = 0    
+
+            self.loss = 0
             if self.verbose:
-                print("Epoch = ", epoch, "\n\n") 
+                print("Epoch = ", epoch, "\n\n")
 
             for x, y in self.data:
                 y_pred = self.__predict_binary(x)
-                
-                if y!= y_pred:
+
+                if y != y_pred:
                     error = y - y_pred
                     self.loss += 1
                     if self.verbose:
                         print("Found Mismatch! Old values are: ")
                         self.__display_weight_binary(self.w, self.b)
-                        print("Updating the values: Adding W", y, " and Subtracting W", y_pred)
-                    
+                        print("Updating the values: Adding W",
+                              y, " and Subtracting W", y_pred)
+
                     # update bias and weight
                     self.b = self.b + self.l_rate * error
                     for i in range(len(x)):
                         self.w[i] = self.w[i] + self.l_rate * error * x[i]
-            
-            if self.verbose: 
-                print("Loss Values ", self.loss, self.min_loss) 
+
+            if self.verbose:
+                print("Loss Values ", self.loss, self.min_loss)
 
             # keeps track of min loss and corresponding weight and bias
             if self.loss < self.min_loss:
@@ -133,28 +135,26 @@ class Perceptron:
                 break
             if self.save_wt:
                 wt_matrix.append(self.w)
-        
+
         # restore the values of optimal weight and bias
         self.w = self.checkw
         self.b = self.checkb
-        
-        
+
         return wt_matrix
-  
+
     def __fit_multiclass(self, X, Y):
 
-        wt_matrix = []        
+        wt_matrix = []
         self.data = list(zip(X, Y))
 
         # Initialise the weight and bias randomly
-        self.w = [[random.random() for i in range(len(X[0]))] for j in range(self.classes)]
+        self.w = [[random.random() for i in range(len(X[0]))]
+                  for j in range(self.classes)]
         self.b = [random.random() for i in range(self.classes)]
 
-
-            
         # set the loss to be maximum as initial value
         self.min_loss = len(X)
-        
+
         # save the weight and bias that gives the minimum loss(no of misclassified points)
         self.checkw = self.w
         self.checkb = self.b
@@ -170,10 +170,10 @@ class Perceptron:
             # shuffle the input randomly
             if self.shuffle:
                 random.shuffle(self.data)
-            
-            self.loss = 0  
+
+            self.loss = 0
             if self.verbose:
-                print("Epoch = ", epoch, "\n\n") 
+                print("Epoch = ", epoch, "\n\n")
             for x, y in self.data:
                 y_pred = self.__predict_multiclass(x)
 
@@ -181,19 +181,19 @@ class Perceptron:
                     self.loss += 1
 
                     # if self.verbose:
-                        # print("Found Mismatch! Old values are: ")
-                        # self.__display_weight_multiclass(self.w, self.b)
-                        # print("Updating the values: Adding W", y, " and Subtracting W", y_pred)
+                    # print("Found Mismatch! Old values are: ")
+                    # self.__display_weight_multiclass(self.w, self.b)
+                    # print("Updating the values: Adding W", y, " and Subtracting W", y_pred)
                     # update bias and weight
-                    
+
                     self.b[y_pred] -= self.l_rate
                     self.b[y] += self.l_rate
                     for i in range(len(x)):
                         self.w[y_pred][i] -= self.l_rate * x[i]
                         self.w[y][i] += self.l_rate * x[i]
 
-            if self.verbose: 
-                print("Loss Values ", self.loss, self.min_loss) 
+            if self.verbose:
+                print("Loss Values ", self.loss, self.min_loss)
             # keeps track of min loss and corresponding weight and bias
             if self.loss < self.min_loss:
                 self.min_loss = self.loss
@@ -212,22 +212,21 @@ class Perceptron:
                 break
             if self.save_wt:
                 wt_matrix.append(self.w)
-            
-            
+
         # restore the values of optimal weight and bias
         self.w = self.checkw
         self.b = self.checkb
-        
+
         return wt_matrix
 
     def fit(self, X, Y, ret_wt_matrix=False):
         # fits the perceptron model
-        
+
         self.save_wt = ret_wt_matrix
-        
+
         # get the number of classes so that appropriate algo can be applied
         self.classes = len(set(Y))
-    
+
         if self.classes == 2:
             print("Binary classification...\n")
             wt_matrix = self.__fit_binary(X, Y)
@@ -238,7 +237,7 @@ class Perceptron:
         # whether to return the saved weight values
         if self.save_wt:
             return wt_matrix
-            
+
     def predict(self, X):
         # predicts the class for a single input matrix X
         y_pred = []
@@ -249,7 +248,7 @@ class Perceptron:
             for x in X:
                 y_pred.append(self.__predict_multiclass(x))
         return y_pred
-    
+
     def accuracy(self, Y, Y_pred):
         # Computes the accuracy score
         count = 0
@@ -263,5 +262,3 @@ class Perceptron:
             self.__display_weight_binary(w, b)
         else:
             self.__display_weight_multiclass(w, b)
-
-    
